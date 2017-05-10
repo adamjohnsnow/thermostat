040c73e3105a7c38c167d97d286dd742581cec95
Adam Snow <adamjohnsnow@icloud.com>
2017-05-10 12:12:08 +0100

    add up & down functions

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,9 +1,31 @@
 describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  })
+
   describe('#defaultTemperature', function() {
-    var thermostat = new Thermostat();
 
     it('has a default temperature of 20', function() {
       expect(thermostat.temperature).toEqual(20);
+    })
+  })
+
+  describe('#temperatureUp', function() {
+
+    it('goes up by one degree', function() {
+      thermostat.temperatureUp();
+      expect(thermostat.temperature).toEqual(21);
+    })
+  })
+
+  describe('#temperatureDown', function() {
+
+    it('goes down by one degree', function() {
+      thermostat.temperatureDown();
+      expect(thermostat.temperature).toEqual(19);
     })
   })
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temperature = 20;
+}
+
+Thermostat.prototype.temperatureUp = function () {
+  this.temperature += 1
+};
+
+Thermostat.prototype.temperatureDown = function () {
+  this.temperature -= 1
 };
