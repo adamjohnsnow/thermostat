ad9bfac9dd08b45307b0331779ed9228dfa5f0d4
Adam Snow <adamjohnsnow@icloud.com>
2017-05-11 16:00:34 +0100

    refactored

--- jquery.js
+++ jquery.js
@@ -1,49 +1,42 @@
 var thermostat = new Thermostat()
 var string = "ON"
 
-function resetDisplay() {
-  $(function() {
+$(function() {
+
+  function resetDisplay() {
     $("#temperature").html(thermostat.temperature + "&deg")
     $("#powerSave").html("POWER-SAVE: " + string);
     $("#energyUsage").text((thermostat.checkUsage()).toUpperCase())
     $("#full-display").css('background', displayColour())
-  });
-};
+  };
 
-resetDisplay();
+  function displayColour() {
+    if (thermostat.checkUsage() == 'high-usage' ) { return '#ff4039' }
+    if (thermostat.checkUsage() == 'medium-usage' ) { return '#3ac7ff' }
+    if (thermostat.checkUsage() == 'low-usage' ) { return '#77f442' }
+  };
 
-function displayColour() {
-  if (thermostat.checkUsage() == 'high-usage' ) { return '#ff4039' }
-  if (thermostat.checkUsage() == 'medium-usage' ) { return '#3ac7ff' }
-  if (thermostat.checkUsage() == 'low-usage' ) { return '#77f442' }
-};
-
-$(function() {
   $("#up").click(function() {
     thermostat.temperatureUp();
     resetDisplay();
   });
-});
 
-$(function() {
   $("#down").click(function() {
     thermostat.temperatureDown();
     resetDisplay();
   });
-});
 
-$(function() {
   $("#reset").click(function() {
     thermostat.resetTemperature();
     resetDisplay();
   });
-});
 
-$(function() {
   $("#power-save").click(function() {
     if (thermostat.powerSave.isOn == true) { thermostat.powerSave.turnOff() }
     else { thermostat.powerSave.turnOn() };
     string = (thermostat.powerSave.isOn == true) ? "ON" : "OFF"
     resetDisplay();
   });
+
+  resetDisplay();
 });
--- src/PowerSave.js
+++ src/PowerSave.js
@@ -2,12 +2,15 @@ function PowerSave() {
   this.turnOn();
 }
 
+PowerSave.ON_MAX_TEMPERATURE = 25
+PowerSave.OFF_MAX_TEMPERATURE = 32
+
 PowerSave.prototype.turnOff = function() {
   this.isOn = false;
-  this.maxTemperature = 32;
+  this.maxTemperature = PowerSave.OFF_MAX_TEMPERATURE;
 }
 
 PowerSave.prototype.turnOn = function() {
   this.isOn = true;
-  this.maxTemperature = 25;
+  this.maxTemperature = PowerSave.ON_MAX_TEMPERATURE;
 }
